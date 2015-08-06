93d0a4f61d05f3f970f6fcbde7bea856eba7f323
tomoima525 <tomoima525@gmail.com>
2015-08-07 01:44:53 +0900

    userdetail

---
+++ app/src/main/java/com/tomoima/cleanarchitecturesample/models/Repos.java
@@ -0,0 +1,10 @@
+package com.tomoima.cleanarchitecturesample.models;
+
+/**
+ * Created by tomoaki on 8/7/15.
+ */
+public class Repos {
+    public String name;
+    public String description;
+    public String html_url;
+}
--- app/src/main/java/com/tomoima/cleanarchitecturesample/models/User.java
+++ app/src/main/java/com/tomoima/cleanarchitecturesample/models/User.java
@@ -1,9 +1,11 @@
 package com.tomoima.cleanarchitecturesample.models;
 
+import java.io.Serializable;
+
 /**
  * Created by tomoaki on 7/25/15.
  */
-public class User {
+public class User implements Serializable{
     public String login;
     public String avatar_url;
 }
